16520f4a87b044f73380359ee02817aa88ca8776
ghirr <islemghariani.dev@gmail.com>
2024-12-17 13:23:16 +0100

    ajouter le fichier help contenant la description de script et créer la fonction help qui affiche le contenu de fichier

--- system-monitor.sh
+++ system-monitor.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+HELP_FILE="help.txt"
 
 # Fonction pour afficher l'utilisation
 show_usage() {
     echo "system-monitor: [-h|--help] [-T] [-t] [-n] [-N] [-d] [-m] [-s] chemin.."
+}
+
+# Fonction pour afficher l'aide à partir de fichier help.txt
+HELP() {
+    cat "$HELP_FILE"
 }
 
 if [[ $# -lt 1 ]]; then
