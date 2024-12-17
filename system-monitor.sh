8385dd4061d635815f876f6d46364613eab96628
ghirr <islemghariani.dev@gmail.com>
2024-12-17 14:08:12 +0100

    creation de fonction monitor_memory qui affiche les quantités totales de mémoire et de zone de swap et les stockent dans un fichier log surveillance.log avec la date d'ajout et recupérer les options avec getopts

--- system-monitor.sh
+++ system-monitor.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 HELP_FILE="help.txt"
+LOG_FILE="/var/log/surveillance.log"
 
 # Fonction pour afficher l'utilisation
 show_usage() {
@@ -12,7 +13,21 @@ HELP() {
     cat "$HELP_FILE"
 }
 
+# Fonction pour la surveillance de la mémoire et du swap
+monitor_memory() {
+    echo "$(date): Surveillance de la mémoire" >> "$LOG_FILE"
+    free -h >> "$LOG_FILE"
+    free -h
+}
+
 if [[ $# -lt 1 ]]; then
     show_usage
     exit 1
 fi
+
+while getopts "hm" opt; do
+    case $opt in
+        h) HELP ;;
+        m) monitor_memory ;;
+    esac
+done
