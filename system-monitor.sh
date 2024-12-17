63b67c1ebbdaa5f0384e10253916f733646ef907
ghirr <islemghariani.dev@gmail.com>
2024-12-17 13:06:42 +0100

    tester l'existance d'un argument sinon afficher l'usage de l'app

--- system-monitor.sh
+++ system-monitor.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+
+# Fonction pour afficher l'utilisation
+show_usage() {
+    echo "system-monitor: [-h|--help] [-T] [-t] [-n] [-N] [-d] [-m] [-s] chemin.."
+}
+
+if [[ $# -lt 1 ]]; then
+    show_usage
+    exit 1
+fi
